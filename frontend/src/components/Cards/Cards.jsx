bb3f70083a7e254f48e12cc7dfb5a335a8027752
NenadJovcic <nenad.hif@hotmail.se>
2023-01-16 19:54:20 +0100

    started on cards skeleton build

--- frontend/src/components/Cards/Cards.jsx
+++ frontend/src/components/Cards/Cards.jsx
@@ -1,10 +1,16 @@
-import './Cards.css'
+import "./Cards.css";
 const Cards = () => {
-    return ( 
-        <div className="cards-container">
-            
-        </div>
-     );
-}
- 
+  return (
+    <div className="cards-container">
+    <div style={{position: 'relative'}}>
+      <img
+        src="https://www.vibe.com/wp-content/uploads/2016/03/Chris-Brown-.png"
+        alt="chris-brown image"
+      />
+      <p className="cards-name">Chris Brown</p>
+      </div>
+    </div>
+  );
+};
+
 export default Cards;
