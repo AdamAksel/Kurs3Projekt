7ccc1ce738aae9cf84dfea9bf4206788ace0d1ae
David-refai <david.refai@hotmail.com>
2023-01-17 21:46:03 +0100

    create a file calendar Card for designing the results of the events, and create Calendar.css for styling all the calendar files, I installed font awesome so I can put icons

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import "./Calendar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendarDays,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 const Calendar = () => {
   return (
-    <>
-    <h1>Calendar</h1>
-    </>
-  )
-}
+    <div className="container_calendar">
+      <div className="container_calendar_nav">
+        <h1 className="calendar_title">Calendar</h1>
+        <div className="calendar_nav">
+          <FontAwesomeIcon icon={faCalendarDays} className="searchIcon" />
+          <span className="calendar_log"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Calendar
+export default Calendar;
