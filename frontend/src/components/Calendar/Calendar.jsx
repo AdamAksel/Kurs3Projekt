37f3fcef9236a9f471ec002fbb4fdf127320c7a4
David-refai <david.refai@hotmail.com>
2023-01-18 02:16:31 +0100

    I have modified the NavBar component jsx/css temporarily to fit the page, I installed the react-rang-date library then I implement the date range in the Calender component

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -1,20 +1,76 @@
 import React from "react";
+import { DateRange, DateRangePicker } from 'react-date-range';
+import { format } from "date-fns";
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 import "./Calendar.css";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCalendarDays,
   faMagnifyingGlass,
 } from "@fortawesome/free-solid-svg-icons";
 const Calendar = () => {
+
+
+  const data = [
+    { date: "2023/01/18", name: "John" },
+    { date: "2023/01/27", name: "Jane" },
+    { date: "2021-02-01", name: "Bob" },
+    { date: "2021-03-01", name: "Sara" },
+  ];
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [filteredData, setFilteredData] = useState([]);
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges) => {
+    let filteredData = data.filter((item) => {
+      let date = new Date(item.date);
+      return (
+        date >= ranges.selection.startDate && date <= ranges.selection.endDate
+      );
+    });
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+    setFilteredData(filteredData);
+  };
   return (
+    <div className="calendar">
+
+   
     <div className="container_calendar">
       <div className="container_calendar_nav">
         <h1 className="calendar_title">Calendar</h1>
         <div className="calendar_nav">
           <FontAwesomeIcon icon={faCalendarDays} className="searchIcon" />
+          <span className="searchText">
+            {format(startDate, "yyyy/MM/dd")} → {format(endDate, "yyyy/MM/dd")}
+          </span>
+     
           <span className="calendar_log"></span>
+
+          
         </div>
+  
       </div>
+    </div>
+    <DateRangePicker
+            editableDateInputs={true}
+            months={2}
+            direction="horizontal"
+            onChange={handleSelect}
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            moveRangeOnFirstSelection={false}
+            className="date"
+          />
     </div>
   );
 };
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
-import { NavLink, Outlet } from 'react-router-dom'
-import './Navbar.css'
-import Sidebar from './Sidebar/Sidebar'
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import "./Navbar.css";
+import Sidebar from "./Sidebar/Sidebar";
 
 const Navbar = () => {
   return (
     <>
-      <header className='Navbar-header'>
-        <nav className='Navbar-nav'>
-          <NavLink to='/'>Home</NavLink>
-          <NavLink to='/Calendar'>Calendar</NavLink>
-          <NavLink to='/Login'>Login</NavLink>
+        <nav className="Navbar-nav">
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+
+            <li>
+              <NavLink to="/Calendar">Calendar</NavLink>
+            </li>
+
+            <li>
+              <NavLink to="/Login">Login</NavLink>
+            </li>
+          </ul>
         </nav>
-      </header>
-      <div className='Navbar-div-container'>
-        <Sidebar />
-        <main className='Navbar-main'>
+  
+      <div className="Navbar-div-container">
+        {/* <Sidebar /> */}
+        <main className="Navbar-main">
           <Outlet />
         </main>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
