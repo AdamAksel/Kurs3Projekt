30831ec6c4bc51e208540766cc1838ec762e6caa
EmilNilsson00 <emilnilsson00@icloud.com>
2023-01-20 15:14:18 +0100

    further styled sidebar, Added more cards, made sidebar sticky, redesigned the cards a little bit

--- frontend/src/components/Navbar/Sidebar/Sidebar.jsx
+++ frontend/src/components/Navbar/Sidebar/Sidebar.jsx
@@ -9,16 +9,62 @@ const Sidebar = () => {
                 <div className="Sidebar-TodaysEvents">
                     <div className="Sidebar-TodaysEvents-Cards">
                         <div className="Sidebar-Card">
-                             <img className="Sidebar-Card-Img" src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/633/25/633-25-101L/Kings-Van-Orton-Design-Poster.jpg"/>
+                            <img className="Sidebar-Card-Img" src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/633/25/633-25-101L/Kings-Van-Orton-Design-Poster.jpg" />
+                            <div className="Sidebar-Card-Info"> Biggie N' 2pac </div>
+                            <div className="Sidebar-Card-Info"><b>SJÖBO</b>, Parken</div>
+                        </div>
+                        <div className="Sidebar-Card">
+                            <img className="Sidebar-Card-Img" src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/633/25/633-25-101L/Kings-Van-Orton-Design-Poster.jpg" />
+                            <div className="Sidebar-Card-Info"> Biggie N' 2pac </div>
+                            <div className="Sidebar-Card-Info"><b>SJÖBO</b>, Parken</div>
+                        </div>
+                        <div className="Sidebar-Card">
+                            <img className="Sidebar-Card-Img" src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/633/25/633-25-101L/Kings-Van-Orton-Design-Poster.jpg" />
+                            <div className="Sidebar-Card-Info"> Biggie N' 2pac </div>
+                            <div className="Sidebar-Card-Info"><b>SJÖBO</b>, Parken</div>
+                        </div>
+                        <div className="Sidebar-Card">
+                            <img className="Sidebar-Card-Img" src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/633/25/633-25-101L/Kings-Van-Orton-Design-Poster.jpg" />
+                            <div className="Sidebar-Card-Info"> Biggie N' 2pac </div>
+                            <div className="Sidebar-Card-Info"><b>SJÖBO</b>, Parken</div>
+                        </div>
+                        <div className="Sidebar-Card">
+                            <img className="Sidebar-Card-Img" src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/633/25/633-25-101L/Kings-Van-Orton-Design-Poster.jpg" />
+                            <div className="Sidebar-Card-Info"> Biggie N' 2pac </div>
+                            <div className="Sidebar-Card-Info"><b>SJÖBO</b>, Parken</div>
+                        </div>
+                        <div className="Sidebar-Card">
+                            <img className="Sidebar-Card-Img" src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/633/25/633-25-101L/Kings-Van-Orton-Design-Poster.jpg" />
+                            <div className="Sidebar-Card-Info"> Biggie N' 2pac </div>
+                            <div className="Sidebar-Card-Info"><b>SJÖBO</b>, Parken</div>
+                        </div>
+                        <div className="Sidebar-Card">
+                            <img className="Sidebar-Card-Img" src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/633/25/633-25-101L/Kings-Van-Orton-Design-Poster.jpg" />
                             <div className="Sidebar-Card-Info"> Biggie N' 2pac </div>
                             <div className="Sidebar-Card-Info"><b>SJÖBO</b>, Parken</div>
                         </div>
                     </div>
                 </div>
-                <br/>
+                <br />
                 <div className="Sidebar-RecentlyAdded">
                     <div className="Sidebar-RecentlyAdded-Text"> Recently Added Events </div>
-                    <div className="Sidebar-RecentlyAdded-Cards"> </div>
+                    <div className="Sidebar-RecentlyAdded-Cards">
+                        <div className="Sidebar-Card">
+                            <img className="Sidebar-Card-Img" src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/633/25/633-25-101L/Kings-Van-Orton-Design-Poster.jpg" />
+                            <div className="Sidebar-Card-Info"> Biggie N' 2pac </div>
+                            <div className="Sidebar-Card-Info"><b>SJÖBO</b>, Parken</div>
+                        </div>
+                        <div className="Sidebar-Card">
+                            <img className="Sidebar-Card-Img" src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/633/25/633-25-101L/Kings-Van-Orton-Design-Poster.jpg" />
+                            <div className="Sidebar-Card-Info"> Biggie N' 2pac </div>
+                            <div className="Sidebar-Card-Info"><b>SJÖBO</b>, Parken</div>
+                        </div>
+                        <div className="Sidebar-Card">
+                            <img className="Sidebar-Card-Img" src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/633/25/633-25-101L/Kings-Van-Orton-Design-Poster.jpg" />
+                            <div className="Sidebar-Card-Info"> Biggie N' 2pac </div>
+                            <div className="Sidebar-Card-Info"><b>SJÖBO</b>, Parken</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
