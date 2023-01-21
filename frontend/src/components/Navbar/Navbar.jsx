6761a97fcb457cef0dcc9ec200c635d127696ad1
Daivd-refai <david.refai@hotmail.com>
2023-01-21 22:48:26 +0100

    I have styled the logo of login with fontawesome

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import { NavLink, Outlet } from 'react-router-dom'
-import './Navbar.css'
-import Sidebar from './Sidebar/Sidebar'
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import "./Navbar.css";
+import Sidebar from "./Sidebar/Sidebar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faCircleUser,
+  faCoffee,
+  faHamburger,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 /*
 <NavLink
@@ -14,47 +22,51 @@ import Sidebar from './Sidebar/Sidebar'
 const Navbar = () => {
   return (
     <>
-      <header className='Navbar-sticky'>
-        <nav className='Navbar-nav'>
-          <div className='Navbar-nav-item'>
+      <header className="Navbar-sticky">
+        <nav className="Navbar-nav">
+          <div className="Navbar-nav-item">
             <NavLink
-              style={{ color: 'inherit', textDecoration: 'inherit' }}
-              to='/'
+              style={{ color: "inherit", textDecoration: "inherit" }}
+              to="/"
             >
-              <i className='lni lni-home'></i>
+              <i className="lni lni-home"></i>
             </NavLink>
           </div>
 
           <NavLink
-            style={{ color: 'inherit', textDecoration: 'inherit' }}
-            to='/Calendar'
+            style={{ color: "inherit", textDecoration: "inherit" }}
+            to="/Calendar"
           >
-            <i class='lni lni-calendar'></i>
+            <i class="lni lni-calendar"></i>
           </NavLink>
           <NavLink
-            style={{ color: 'inherit', textDecoration: 'inherit' }}
-            to='/Login'
+            style={{ color: "inherit", textDecoration: "inherit" }}
+            to="/Login"
           >
-            <i class='lni lni-user'></i>
+            <div className="login">
+              <FontAwesomeIcon icon={faCircleUser} className="icon iconUser" />
+              <FontAwesomeIcon icon={faBars} className="icon iconBars" />
+            </div>
+            {/* <i class="lni lni-user"></i> */}
           </NavLink>
         </nav>
       </header>
-      <header className='Navbar-header'>
-        <div className='Navbar-search-image'>
-          <div className='Navbar-search'>
-            <input className='Navbar-search-input' />
-            <button className='Navbar-search-button'>Search</button>
+      <header className="Navbar-header">
+        <div className="Navbar-search-image">
+          <div className="Navbar-search">
+            <input className="Navbar-search-input" />
+            <button className="Navbar-search-button">Search</button>
           </div>
         </div>
       </header>
-      <div className='Navbar-div-container'>
+      <div className="Navbar-div-container">
         <Sidebar />
-        <main className='Navbar-main'>
+        <main className="Navbar-main">
           <Outlet />
         </main>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
