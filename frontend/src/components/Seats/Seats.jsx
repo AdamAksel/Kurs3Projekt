e8f03658c25118f27a389ed325d97d4e5a294284
EdvinN1 <nedde14@gmail.com>
2023-01-23 15:30:03 +0100

    Second commit

--- frontend/src/components/Seats/Seats.jsx
+++ frontend/src/components/Seats/Seats.jsx
@@ -6,14 +6,27 @@ const Seats = () => {
     <>
       <h1>Seats</h1>
 
-      <div className='circle'>
+     <div className='seat-container'>
+        <div className="seat-column-1">SEKT 1</div>
+        <div className="seat-column-2">
+        <div className='seat-middle-column-1'>SCEN</div>
+        <div className='seat-middle-column-2'></div>
+        <div className='seat-middle-column-3'></div>
+        </div>
+        <div className="seat-column-1">SEKT 2</div>
+     </div>
+     
+     
+     
+     
+      {/*<div className='circle'>
         
   <div>1</div>
   <div>2</div>
   <div>3</div>
   <div>4</div>
   <div className='middle-circle'>hej</div>
-</div>
+  </div>*/}
     </>
   )
 }
