e5ce3a2aac594f1c95bcb143bc39f64959d2bdad
EdvinN1 <nedde14@gmail.com>
2023-01-23 14:08:52 +0100

    First commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Start, Navbar, Calendar, Login } from './components'
+import { Start, Navbar, Calendar, Login, Seats } from './components'
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -13,6 +13,7 @@ const router = createBrowserRouter(
       <Route index element={<Start />} />
       <Route path='/Calendar' element={<Calendar />} />
       <Route path='/Login' element={<Login />} />
+      <Route path='/Seats' element={<Seats />} />
     </Route>
   )
 )
---
+++ frontend/src/components/Seats/Seats.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Seats.css";
+
+const Seats = () => {
+  return (
+    <>
+      <h1>Seats</h1>
+
+      <div className='circle'>
+        
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div className='middle-circle'>hej</div>
+</div>
+    </>
+  )
+}
+
+export default Seats
