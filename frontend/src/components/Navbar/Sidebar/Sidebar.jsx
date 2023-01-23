0538f353868bba02dca192b35f42e50ce83fc8cd
EmilNilsson00 <emilnilsson00@icloud.com>
2023-01-23 14:26:50 +0100

    Redesigned sidebar, now being able to switch between the two categories by clicking on the name of the category, instead of showing both categories at the same time. Simply styled scrollbar

--- frontend/src/components/Navbar/Sidebar/Sidebar.jsx
+++ frontend/src/components/Navbar/Sidebar/Sidebar.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Sidebar.css'
 
 const Sidebar = () => {
+
+    const [viewChange, setViewChange] = useState(0)
+
     return (
         <>
-            <div className='Sidebar-Container'>
-                <div className="Sidebar-TodaysEvents-Text"> Today's Events </div>
-                <div className="Sidebar-TodaysEvents">
+            <div className="Sidebar-Container">
+                <div className="Sidebar-Texts">
+                <div style={viewChange === 0 ? {backgroundColor: "#374a64"} : null} onClick={() => {setViewChange(0)}} className="Sidebar-TodaysEvents-Text"> Today's Events </div>
+                <div style={viewChange === 1 ? {backgroundColor: "#374a64"} : null} onClick={() => {setViewChange(1)}} className="Sidebar-RecentlyAdded-Text"> Recently Added </div>
+                </div>
+                {viewChange === 0 ? (<div className="Sidebar-TodaysEvents">
                     <div className="Sidebar-TodaysEvents-Cards">
                         <div className="Sidebar-Card">
                             <img className="Sidebar-Card-Img" src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/633/25/633-25-101L/Kings-Van-Orton-Design-Poster.jpg" />
@@ -44,10 +50,7 @@ const Sidebar = () => {
                             <div className="Sidebar-Card-Info"><b>SJÖBO</b>, Parken</div>
                         </div>
                     </div>
-                </div>
-                <br />
-                <div className="Sidebar-RecentlyAdded">
-                    <div className="Sidebar-RecentlyAdded-Text"> Recently Added Events </div>
+                </div>) : (<div className="Sidebar-RecentlyAdded">
                     <div className="Sidebar-RecentlyAdded-Cards">
                         <div className="Sidebar-Card">
                             <img className="Sidebar-Card-Img" src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/633/25/633-25-101L/Kings-Van-Orton-Design-Poster.jpg" />
@@ -65,7 +68,7 @@ const Sidebar = () => {
                             <div className="Sidebar-Card-Info"><b>SJÖBO</b>, Parken</div>
                         </div>
                     </div>
-                </div>
+                </div>)}
             </div>
         </>
     )
