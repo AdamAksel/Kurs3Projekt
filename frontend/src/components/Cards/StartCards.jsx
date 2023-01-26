4121e36ece0014c414964913d09b6247a7fd1c69
NenadJovcic <nenad.hif@hotmail.se>
2023-01-26 09:03:08 +0100

    bug fix startcards

--- frontend/src/components/Cards/StartCards.jsx
+++ frontend/src/components/Cards/StartCards.jsx
@@ -1,11 +1,11 @@
 import "./StartCards.css";
 
 const StartCards = (props) => {
-  console.log(props)
+  console.log(props);
   return (
     <section className="cards-container">
-      <div style={{ position: "relative", outline: 'none' }}>
-        <div style={{outline:'none'}}>
+      <div style={{ position: "relative", outline: "none" }}>
+        <div style={{ outline: "none" }}>
           <img
             src="https://www.vibe.com/wp-content/uploads/2016/03/Chris-Brown-.png"
             alt="chris-brown image"
@@ -15,13 +15,16 @@ const StartCards = (props) => {
           </div>
         </div>
 
-        
-        <p className="date">Date: 2023-01-20</p>
-        <p className="time">Time: 18:00-22:00</p>
-        <p className="location" style={{ fontWeight: "bold" }}>
-          Helsingborg:{" "}
-          <span style={{ fontWeight: "normal", outline: "none" }}>Olympia</span>
-        </p>
+        <p className="start-cards-date">Date: 2023-01-20</p>
+        <div style={{ fontSize: "15px" }}>
+          <p className="time">Time: 18:00-22:00</p>
+          <p className="location" style={{ fontWeight: "bold" }}>
+            Helsingborg:{" "}
+            <span style={{ fontWeight: "normal", outline: "none" }}>
+              Olympia
+            </span>
+          </p>
+        </div>
       </div>
     </section>
   );
