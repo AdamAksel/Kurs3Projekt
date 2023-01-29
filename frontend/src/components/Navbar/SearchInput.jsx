521fc81fc12e281b77b9a80db25b7d5652d45737
NenadJovcic <nenad.hif@hotmail.se>
2023-01-29 13:24:20 +0100

    added link to event page on searchresult

--- frontend/src/components/Navbar/SearchInput.jsx
+++ frontend/src/components/Navbar/SearchInput.jsx
@@ -4,49 +4,46 @@ import React from "react";
 import { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
 
 
 export default function SearchInput({ events }) {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  
+
   const handleSearch = (e) => {
     const searchTerm = e.target.value.toLowerCase();
     setSearchResults([]);
-  
+
     if (searchTerm === "") {
       setSearchResults([]);
       return;
     }
-  
-    let arrayTest = [];
-    for(let i = 0; i < events.length; i++){
-      if (events[i].artist.toLowerCase().includes(searchTerm)) {
-        arrayTest.push({value: events[i].artist, id: events[i].id})
-      }
-      if (events[i].eventName.toLowerCase().includes(searchTerm)) {
-        arrayTest.push({value: events[i].eventName, id: events[i].id})
-      }
-    }
-    
+
+    // let arrayTest = [];
+    // for (let i = 0; i < events.length; i++) {
+      
+    //   if (events[i].artist.toLowerCase().includes(searchTerm)) {
+    //     arrayTest.push({ value: events[i].artist, id: events[i].id });
+    //   }
+    //   if (events[i].eventName.toLowerCase().includes(searchTerm)) {
+    //     arrayTest.push({ value: events[i].eventName, id: events[i].id });
+    //   }
+    // }
+
     let filteredEvents = [];
-     events.forEach(element => {
+    events.forEach((element) => {
       
       if (element.artist.toLowerCase().includes(searchTerm)) {
-        displayArray.push({value: element.artist, id: element.id})
+        filteredEvents.push({ value: element.artist, id: element.id });
       }
-      
+
       if (element.eventName.toLowerCase().includes(searchTerm)) {
-        displayArray.push({value: element.eventName, id: element.id})
+        filteredEvents.push({ value: element.eventName, id: element.id });
       }
     });
-    console.log(filteredEvents)
-      setSearchResults(filteredEvents)
+    console.log(filteredEvents);
+    setSearchResults(filteredEvents);
   };
-
-   
- 
 
   return (
     <div className="Navbar-search-container">
@@ -68,17 +65,14 @@ export default function SearchInput({ events }) {
         <div className="search-results">
           {searchResults.map((event) => {
             const key = `${event.id}-${event.value}`;
-            
+
             return (
               <Link
-                to={`/search-results/${event.id}`}
+                to={`/Event/${event.value}`}
                 key={key}
                 style={{ textDecoration: "none", color: "black" }}
               >
-                <div className="search-results-links">
-                  {event.value}
-                </div>
-                
+                <div className="search-results-links">{event.value}</div>
               </Link>
             );
           })}
