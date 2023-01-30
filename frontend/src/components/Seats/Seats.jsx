7ecfa7c3d509a699402b83b2c429c72abce5e9f4
NenadJovcic <nenad.hif@hotmail.se>
2023-01-30 21:47:40 +0100

    test

--- frontend/src/components/Seats/Seats.jsx
+++ frontend/src/components/Seats/Seats.jsx
@@ -30,11 +30,17 @@ class BuyTickets extends React.Component {
 
 export default class SeatSelection extends React.Component {
   seats = [
-    "A1", "A2", "A3", "A4", "A5",
-    "B1", "B2", "B3", "B4", "B5",
-    "C1", "C2", "C3", "C4", "C5",
-    "D1", "D2", "D3", "D4", "D5",
-    "E1", "E2", "E3", "E4", "E5",
+    "A1", "A2", "A3", "A4", "A5", "B4", "B5",
+    "B1", "B2", "B3", "B4", "B5", "B4", "B5",
+    "C1", "C2", "C3", "C4", "C5", "B4", "B5",
+    "D1", "D2", "D3", "D4", "D5", "B4", "B5",
+    "E1", "E2", "E3", "E4", "E5", "B4", "B5",
+    "F1", "F2", "F3", "F4", "F5", "B4", "B5",
+    "G1", "G2", "G3", "G4", "G5", "B4", "B5",
+    "G1", "G2", "G3", "G4", "G5", "B4", "B5",
+    // "H1", "H2", "H3", "H4", "H5",
+    // "I1", "I2", "I3", "I4", "I5",
+    // "J1", "J2", "J3", "J4", "J5",
   ];
 
   state = {
@@ -50,25 +56,43 @@ export default class SeatSelection extends React.Component {
   render() {
     return (
       <div className="container">
-      <div className="container-wrap">
-        <ol>
-          <li className="row row--1">
-            <ol className="seats" type="A">
-              {this.seats.map((seat) => (
-                <li className="seat" key={seat}>
-                  <input
-                    type="checkbox"
-                    id={seat}
-                    onClick={this.handleInputClick}
-                  />
-                  <label for={seat}>{seat}</label>
-                </li>
-              ))}
-            </ol>
-          </li>
-        </ol>
+        <div style={{width: "50%", float: "left"}}>
+          <ol className="container-wrap">
+            <li className="">
+              <ol className="seats" type="A">
+                {this.seats.slice(0, Math.ceil(this.seats.length / 2)).map((seat) => (
+                  <li className="seat" key={seat}>
+                    <input
+                      type="checkbox"
+                      id={seat}
+                      onClick={this.handleInputClick}
+                    />
+                    <label for={seat}>{seat}</label>
+                  </li>
+                ))}
+              </ol>
+            </li>
+          </ol>
+        </div>
+        <div style={{width: "50%", float: "left"}}>
+          <ol className="container-wrap">
+            <li className="">
+              <ol className="seats" type="A">
+                {this.seats.slice(Math.ceil(this.seats.length / 2)).map((seat) => (
+                  <li className="seat" key={seat}>
+                    <input
+                      type="checkbox"
+                      id={seat}
+                      onClick={this.handleInputClick}
+                    />
+                    <label for={seat}>{seat}</label>
+                  </li>
+                ))}
+              </ol>
+            </li>
+          </ol>
+        </div>
         <BuyTickets ticketCount={this.state.ticketCount} />
-      </div>
       </div>
     );
   }
