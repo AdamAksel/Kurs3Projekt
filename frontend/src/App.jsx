934c79dbff9e5f48bbe27980a926f727ea7af164
EmilNilsson00 <emilnilsson00@icloud.com>
2023-02-01 15:58:05 +0100

    Manuel fetched data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Navbar } from './components'
 import { Outlet } from 'react-router'
+import {GlobalProvider} from "./GlobalContext.jsx";
 
 function App() {
   return (
-    <>
+    <GlobalProvider>
     <header className="header_app">
       <Navbar />
       </header>
       <main className="main_app">
       <Outlet />
       </main>
-    </>
+    </GlobalProvider>
   )
 }
 
---
+++ server/README.md
@@ -0,0 +1,1 @@
+# live-fanatic-backend
