150203e8e4555a76124d8624728facba71faad8f
Adam Carlsson <akseltheaustrian@gmail.com>
2023-02-03 13:28:05 +0100

    trying to clean up calendar and map concerts 10 at a time

--- frontend/src/components/Calendar/CalenderCard.jsx
+++ frontend/src/components/Calendar/CalenderCard.jsx
@@ -1,102 +1,38 @@
-
-
-export default function CalenderCard({ events }) {
+export default function CalenderCard({ event }) {
   // Map the events to the DOM
-  const highlight = "highlight";
-
-  const handleEventsName = (name) => {
-    const splitName = name.split(" ");
-    const firstWord = splitName[0];
-    const secondWord = splitName[1];
-    return (
-      <div className="event__title">
-        <h1>
-          <span className="highlight">{firstWord}</span>
-          <br />
-          <span className={highlight}>{secondWord}</span>
-        </h1>
-      </div>
-    );
-  };
 
   return (
-    events &&
-    events.map((event, i) => {
-      let firstDate = new Date(event.date[0]);
-
-      let lastDate = new Date(event.date[event.date.length - 1]);
-
-      const markup = [firstDate, lastDate];
-
-      let classes = "";
-      event.date.length > 1
-        ? (classes = "event__date--multi")
-        : (classes = "event__date");
-      return (
-        <div className="container__calendar__Card" key={i}>
-          <div className="date_card">
-            {markup.map((event, i) => {
-              let date = new Date(event);
-              const day = date.getDate();
-              const day2 = date.getDay();
-
-              const dayName = ["sön", "mån", "tis", "ons", "tor", "fre", "lör"];
-              const dayN = dayName[day2];
-              const month = date.getMonth();
-              const monthName = [
-                "jan",
-                "feb",
-                "mar",
-                "apr",
-                "maj",
-                "jun",
-                "jul",
-                "aug",
-                "sep",
-                "okt",
-                "nov",
-                "dec",
-              ];
-              const monthN = monthName[month];
-              const year = date.getFullYear();
-              const hour = `${date.getHours()}`.padStart(2, 0);
-              const min = `${date.getMinutes()}`.padStart(2, 0);
-
-              return (
-                <div className="event__date" key={i}>
-                  <div className="event__date_day">{dayN}</div>
-                  <div className="event__date_day_number">{day}</div>
-                  <div className="event__month_year">
-                    <div className="event__date__month">{monthN}.</div>
-                    <div className="event__date__year">{year}</div>
-                  </div>
-                  <div className="event__date__time">{`${hour}:${min}`}</div>
-                </div>
-              );
-            })}
-            <span className="arrow">→</span>
+    <div className='container__calendar__Card'>
+      <div className='date_card'>
+        <div className='event__date'>
+          <div className='event__date_day'>{event.date}</div>
+          <div className='event__date_day_number'>{event.datum}</div>
+          <div className='event__month_year'>
+            <div className='event__date__month'>{event.datum}.</div>
+            <div className='event__date__year'>{event.datum}</div>
           </div>
-          
-          <div className="event__image">
-            <img className="event__img" src={event.image} alt="event" />
-
-            {event.eventName && handleEventsName(event.eventName)}
-          </div>
-          <div className="event__info">
-            <p className="event__description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            </p>
-            <div className="event__location">
-              <p>{event.location}</p>
-              <span>|</span>
-              <p>place</p>
-            </div>
-          </div>
-          <div className="event__payment__button">
-            <button className="event__button">Buy Ticket</button>
-          </div>
+          <div className='event__date__time'>{`${event.hour}:${event.minute}`}</div>
         </div>
-      );
-    })
-  );
+        );
+        <span className='arrow'>→</span>
+      </div>
+
+      <div className='event__image'>
+        <img className='event__img' src={event.image} alt='event' />
+      </div>
+      <div className='event__info'>
+        <p className='event__description'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        </p>
+        <div className='event__location'>
+          <p>{event.venue}</p>
+          <span>|</span>
+          <p>place</p>
+        </div>
+      </div>
+      <div className='event__payment__button'>
+        <button className='event__button'>Buy Ticket</button>
+      </div>
+    </div>
+  )
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,54 +1,54 @@
-import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
-import "./Navbar.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faCircleUser } from "@fortawesome/free-solid-svg-icons";
-import ImageHeader from "./ImageHeader";
-import IconResponsive from "./IconResponsive";
+import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
+import './Navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faCircleUser } from '@fortawesome/free-solid-svg-icons'
+import ImageHeader from './ImageHeader'
+import IconResponsive from './IconResponsive'
 
 const Navbar = () => {
   return (
     <>
-      <header className="header">
-        <div className="nav_flex_1">
-          <NavLink to="/">
+      <header className='header'>
+        <div className='nav_flex_1'>
+          <NavLink to='/'>
             <img
-              src="./logo.svg"
-              alt="vegas"
-              className="navbar__nav__logo__icon"
+              src='./logo.svg'
+              alt='vegas'
+              className='navbar__nav__logo__icon'
             />
           </NavLink>
-          <nav className="navbar__nav">
-            <ul className="navbar__nav__list">
+          <nav className='navbar__nav'>
+            <ul className='navbar__nav__list'>
               <li>
-                <NavLink to="/">Home</NavLink>
+                <NavLink to='/'>Home</NavLink>
               </li>
               <li>
-                <NavLink to="/Calendar">Calendar</NavLink>
+                <NavLink to='/Calendar/0'>Calendar</NavLink>
               </li>
 
-              <li className="login_hover">
-                <NavLink to="/Login">
-                  <div className="login">
+              <li className='login_hover'>
+                <NavLink to='/Login'>
+                  <div className='login'>
                     <FontAwesomeIcon
                       icon={faCircleUser}
-                      className="icon iconUser"
+                      className='icon iconUser'
                     />
-                    <FontAwesomeIcon icon={faBars} className="icon iconBars" />
+                    <FontAwesomeIcon icon={faBars} className='icon iconBars' />
                   </div>
                 </NavLink>
               </li>
             </ul>
-            <div className="responsive">
+            <div className='responsive'>
               <IconResponsive />
             </div>
           </nav>
         </div>
-        <div className="nav_flex_2">
+        <div className='nav_flex_2'>
           <ImageHeader />
         </div>
       </header>
     </>
-  );
-};
-export default Navbar;
+  )
+}
+export default Navbar
