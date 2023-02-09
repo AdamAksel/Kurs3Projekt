8ea59e00f131c8e0952b0505edf4d1f5caa2cb26
EmilNilsson00 <emilnilsson00@icloud.com>
2023-02-09 12:50:35 +0100

    What I managed to refactor

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,70 +1,67 @@
-import React, { useState, useContext } from 'react'
+import React, {useState, useContext} from 'react'
 import './Sidebar.css'
 import SidebarCard from '../Sidebar-Card/Sidebar-Card.jsx'
 import GlobalContext from '../../GlobalContext'
 
-const Sidebar = () => {
-  const { recentlyAddedEvents, todaysEvents } = useContext(GlobalContext)
+export default function () {
+    const {recentlyAddedEvents, todaysEvents} = useContext(GlobalContext)
 
-  const [viewChange, setViewChange] = useState(0)
+    const [viewChange, setViewChange] = useState(0)
 
-  return (
-    <>
-      <aside className='sidebar'>
-        <div className='sidebar-texts'>
-          <p
-            style={viewChange === 0 ? { backgroundColor: '#374a64' } : null}
-            onClick={() => {
-              setViewChange(0)
-            }}
-            className='sidebar-todaysevents-text'
-          >
-            {' '}
-            Today's Events{' '}
-          </p>
-          <p
-            style={viewChange === 1 ? { backgroundColor: '#374a64' } : null}
-            onClick={() => {
-              setViewChange(1)
-            }}
-            className='sidebar-recentlyadded-text'
-          >
-            {' '}
-            Recently Added{' '}
-          </p>
-        </div>
-        {viewChange === 0 ? (
-          <section className='sidebar-todaysevents'>
-            {todaysEvents
-              .filter((event, index) => index < 2)
-              .map((event) => (
-                <div
-                  key={Math.random() * 1000000}
-                  className='sidebar-todaysevents-cards'
-                >
-                  <SidebarCard event={event} />
+    return (
+        <>
+            <aside className='sidebar'>
+                <div className='sidebar-texts'>
+                    <p style={viewChange === 0 ? {backgroundColor: '#374a64'} : null}
+                       onClick={() => {
+                           setViewChange(0)
+                       }}
+                       className='sidebar-button'
+                    >
+                        {' '}
+                        Today's Events{' '}
+                    </p>
+                    <p
+                        style={viewChange === 1 ? {backgroundColor: '#374a64'} : null}
+                        onClick={() => {
+                            setViewChange(1)
+                        }}
+                        className='sidebar-button'
+                    >
+                        {' '}
+                        Recently Added{' '}
+                    </p>
                 </div>
-              ))}
-          </section>
-        ) : (
-          <section className='sidebar-recentlyadded'>
-            <div className='sidebar-recentlyadded-cards'>
-              {recentlyAddedEvents
-                .filter((event, index) => index < 2)
-                .map((event) => (
-                  <div
-                    key={Math.random() * 1000000}
-                    className='sidebar-todaysevents-cards'
-                  >
-                    <SidebarCard event={event} />
-                  </div>
-                ))}
-            </div>
-          </section>
-        )}
-      </aside>
-    </>
-  )
+                {viewChange === 0 ? (
+                    <section className='sidebar-todaysevents'>
+                        {todaysEvents
+                            .filter((event, index) => index < 2)
+                            .map((event) => (
+                                <div
+                                    key={Math.random() * 1000000}
+                                    className='sidebar-todaysevents-cards'
+                                >
+                                    <SidebarCard event={event}/>
+                                </div>
+                            ))}
+                    </section>
+                ) : (
+                    <section className='sidebar-recentlyadded'>
+                        <div className='sidebar-recentlyadded-cards'>
+                            {recentlyAddedEvents
+                                .filter((event, index) => index < 2)
+                                .map((event) => (
+                                    <div
+                                        key={Math.random() * 1000000}
+                                        className='sidebar-todaysevents-cards'
+                                    >
+                                        <SidebarCard event={event}/>
+                                    </div>
+                                ))}
+                        </div>
+                    </section>
+                )}
+            </aside>
+        </>
+    )
 }
-
-export default Sidebar
