4e5ebf9799432abbe181669dfc2db3bc643fd0b7
NenadJovcic <nenad.hif@hotmail.se>
2023-02-09 11:23:37 +0100

    refactored start and startcards

---
+++ frontend/src/components/Start/EventButtons.jsx
@@ -0,0 +1,23 @@
+export default function ({setStartEvents, todaysEvents, featuredEvents, recentlyAddedEvents}) {
+    return <div className="startButtons">
+        <div
+          onClick={() => setStartEvents([...todaysEvents])}
+          className="start-button"
+        >
+          Todays Events
+        </div>
+        <div
+          onClick={() => setStartEvents([...featuredEvents])}
+          className="start-button"
+        >
+          Featured
+        </div>
+        <div
+          onClick={() => setStartEvents([...recentlyAddedEvents])}
+          className="start-button"
+        >
+          Latest Added
+        </div>
+  </div>
+  }
+  
--- frontend/src/components/Start/Start.jsx
+++ frontend/src/components/Start/Start.jsx
@@ -1,55 +1,21 @@
-import React, { useContext, useState, useEffect } from 'react'
-import StartCards from '../Cards/StartCards'
+import React, { useContext, useState, } from 'react'
 import './Start.css'
 import GlobalContext from '../../GlobalContext.jsx'
 import Sidebar from '../Sidebar/Sidebar'
+import StartPageEvents from './StartPageEvents'
+import EventButtons from './EventButtons'
 
-const Start = () => {
-  const { featuredEvents, todaysEvents, recentlyAddedEvents } =
-    useContext(GlobalContext)
+export default function () {
+  const { featuredEvents, 
+    todaysEvents, 
+    recentlyAddedEvents } = useContext(GlobalContext)
 
   const [startEvents, setStartEvents] = useState([])
 
-  return (
-    <>
-      <section className='start-flex'>
-        <Sidebar />
-        <div className='start-buttons-container'>
-          <div
-            onClick={() => setStartEvents([...todaysEvents])}
-            className='start-button'
-          >
-            Todays Events
-          </div>
-          <div
-            onClick={() => setStartEvents([...featuredEvents])}
-            className='start-button'
-          >
-            Featured
-          </div>
-          <div
-            onClick={() => setStartEvents([...recentlyAddedEvents])}
-            className='start-button'
-          >
-            Latest Added
-          </div>
-        </div>
-        <div className='startPage'>
-          {startEvents.length == 0
-            ? featuredEvents.map((event) => (
-                <div className='sect-cards' key={Math.random() * 1000000}>
-                  <StartCards event={event} />
-                </div>
-              ))
-            : startEvents.map((event) => (
-                <div className='sect-cards' key={Math.random() * 1000000}>
-                  <StartCards event={event} />
-                </div>
-              ))}
-        </div>
-      </section>
-    </>
-  )
+  return <section className='start-flex'>
+      <Sidebar />
+      <EventButtons todaysEvents ={todaysEvents} recentlyAddedEvents={recentlyAddedEvents} featuredEvents={featuredEvents} setStartEvents={setStartEvents} />
+      <StartPageEvents startEvents={startEvents} featuredEvents={featuredEvents} />
+  </section>
 }
 
-export default Start
